6a3fa3746bf026e1661ffb7d310c8d6723b8cc79
akshayburjwalpaytm <akshay.burjwal@paytmbank.com>
2019-08-13 00:47:22 +0530

    Base Project Created

---
+++ DynamicPropLoader/src/main/java/com/dynamic/prop/DynamicPropLoader/DynamicPropLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.dynamic.prop.DynamicPropLoader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPropLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicPropLoaderApplication.class, args);
+	}
+
+}
---
+++ DynamicPropLoader/src/test/java/com/dynamic/prop/DynamicPropLoader/DynamicPropLoaderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dynamic.prop.DynamicPropLoader;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DynamicPropLoaderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
